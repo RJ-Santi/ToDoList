1dc7ed214d23ca18e78fb2225bd20ee2079c1196
Ryan Santiago <cr7rs7@gmail.com>
2021-12-22 22:21:39 -0500

    Added local storage functionality.  Fixed a bug where the list would not show until new items were added even if data was in local storage.

--- app.js
+++ app.js
@@ -4,24 +4,88 @@ const button = document.querySelector('.btn');
 const input = document.querySelector('#item');
 const list = document.querySelector('#list');
 
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+  // DOM load event
+  document.addEventListener('DOMContentLoaded', getTasks);
+  document.addEventListener('DOMContentLoaded', tasksExist);
+  form.addEventListener('submit', addTask);
+  button.addEventListener('click', clearTasks);
+}
+
 
 const liMaker = (text) => {
   const li = document.createElement('li');
   li.classList.add('list-group-item', 'col-6', 'mx-auto');
-  li.textContent = text;
+  li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
+
+  // Store in local storage
+  storeTaskInLocalStorage(input.value);
 }
 
-// Event listeners for adding and deleting
-form.addEventListener('submit', function(e) {
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// get tasks from local storage
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach((tasks) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'col-6', 'mx-auto');
+    li.textContent = tasks;
+    ul.appendChild(li);
+  });
+
+}
+
+// adding tasks
+function addTask(e) {
   e.preventDefault();
   list.style.display = 'block';
 
   liMaker(input.value);
   input.value = '';
-});
+};
 
-button.addEventListener('click', function(e) {
-  e.preventDefault();
-  list.innerHTML = '';
-});
+// clearing all tasks
+function clearTasks(e) {
+  while(ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+
+  list.style.display = 'none';
+
+  clearTasksFromLocalStorage();
+}
+
+// clear tasks from local storage
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
+}
+
+// show items if local storage contains them
+function tasksExist() {
+  if(localStorage.getItem('tasks') != null) {
+    list.style.display = 'block';
+  }
+}
